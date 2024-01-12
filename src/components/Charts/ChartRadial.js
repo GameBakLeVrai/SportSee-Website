7ef700d46e0876766a4da63eebd80b59b458aa7d
GameBak <gamebak@null.co>
2024-01-12 12:03:05 +0100

    White circle added from RadialChart Score

--- src/components/Charts/ChartRadial.js
+++ src/components/Charts/ChartRadial.js
@@ -33,16 +33,16 @@ const ChartRadial = () => {
 			className="radialChart"
 			startAngle={90} 
 			endAngle={data[0].value}
-			backgroundColor="#fff"
 		>
 			<text x="30" y="45" fontSize={18} fontWeight={500}>Score</text>
 			<RadialBar minAngle={15} dataKey="value" cornerRadius={5} fill="red" />
-			
-			<text x="30" y="45" fontSize={18} fontWeight={500} className="scorepie">
+
+			<circle cx="50%" cy="50%" r="77" fill="white" />
+			<text x="30" y="45" fontSize={18} fontWeight={500}>
 				<tspan x="125" y="120" fontSize={30} fill={'#282D30'}>{data[0].value-100} %</tspan>
 				<tspan x="118" y="150" fill={'#74798c'}>de votre</tspan>
 				<tspan x="120" y="175" fill={'#74798c'}>objectif</tspan>
-			</text>
+			</text>				
 		</RadialBarChart>
 	);
 };
